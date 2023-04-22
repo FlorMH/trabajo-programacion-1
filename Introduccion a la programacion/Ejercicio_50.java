7d195f4d59205ceceb2e1860bde46b0a1d330f00
FlorMH <125076890+FlorMH@users.noreply.github.com>
2023-04-21 23:29:09 -0600

    Add files via upload

---
+++ Introduccion a la programacion/Ejercicio_50.java
@@ -0,0 +1,23 @@
+
+package ejercicio_flor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jr813
+ */
+public class Ejercicio_50 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int num = sc.nextInt();
+        System.out.println("Los divisores de " + num + " son:");
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
